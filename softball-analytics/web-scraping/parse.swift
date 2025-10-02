5777c938f8aefb0b28c746fd072f82433f6d113b
Josh Smith <jbsmith@hamilton.edu>
2025-10-02 11:10:38 -0400

    Extracting text from PDFs works

--- softball-analytics/web-scraping/parse.swift
+++ softball-analytics/web-scraping/parse.swift
@@ -7,15 +7,15 @@
 //  10/1/25
 //
 import Foundation
-
+import PDFKit
 
 func get_files() -> [String] {
     // Return a list of all PDF filenames from the pdfs folder
     let fileManager = FileManager.default
-    let directoryPath = FileManager.default.currentDirectoryPath + "/pdfs"
+    let directoryPath: String = FileManager.default.currentDirectoryPath + "/pdfs"
 
     do {
-        let contents = try fileManager.contentsOfDirectory(atPath: directoryPath)
+        let contents: [String] = try fileManager.contentsOfDirectory(atPath: directoryPath)
         return contents.sorted()
     } catch {
         print("Error reading directory: \(error)")
@@ -37,12 +37,44 @@ class GameParser {
         self.text = self._get_pdf_text(filename)
     }
 
-    func _get_pdf_text(_ filename: String) -> String {
+    func parse() throws -> String {
+        print(self.text)
         return ""
     }
 
-    func parse() throws -> String {
-        return ""
+    // def _get_pdf_text(self, filename: str) -> str:
+    //     """Get full text from PDF file"""
+    //     reader = PdfReader("pdfs/" + filename)
+    //     text = ""
+    //     for index, page in enumerate(reader.pages):
+    //         if index:
+    //             text += '\n'
+    //         text += page.extract_text()
+        
+    //     return text
+
+    func _get_pdf_text(_ filename: String) -> String {
+        // Get full text from PDF file
+        let fileURL = URL(fileURLWithPath: "pdfs/\(filename)")
+
+        guard let pdfDocument = PDFDocument(url: fileURL) else {
+            print("Failed to load PDF: \(filename)")
+            return ""
+        }
+
+        var text: String = ""
+
+        for page_num: Int in 0..<pdfDocument.pageCount {
+            if let page: PDFPage = pdfDocument.page(at: page_num),
+            let pageText: String = page.string {
+                if page_num > 0 {
+                    text += "\n"
+                }
+                text += pageText
+            }
+        }
+
+        return text
     }
 }
 
@@ -60,6 +92,7 @@ func main() {
             print("failed parsing \(filename)")
             fails += 1
         }
+        break
     }
 
     print("Total fails: \(fails) / \(files.count)")
